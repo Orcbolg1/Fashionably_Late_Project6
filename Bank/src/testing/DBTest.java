3775ba3b9097cf2cd74e9f106ad29a68005aec5e
Michael Tarallo <mtarallo@hawkmail.hccfl.edu>
2019-04-06 23:09:05 -0400

    **See Comments**

--- Bank/src/testing/DBTest.java
+++ Bank/src/testing/DBTest.java
@@ -1,7 +1,7 @@
 /*
  * Group: Fashionably Late
  * ------------------------------------------------------------
- * Version 1.0.0-1:
+ * Version 1.0.0-2:
  * ------------------------------------------------------------
  * Main Class to carry out testing of methods within banking package.
  * 
@@ -25,27 +25,55 @@
  * --Imported Skeletion Classes given for project
  * --Created 'DBTest' class
  * --Imported package 'banking' classes
+ * --Created JUnit 4 for testing
+ * --Added Suppressed Warnings for unused imports
  * 
  * ------------------------------------------------------------
  * TO-DO:
  * --Create a dataset (Map?) with testable data to use when testing methods of the banking package
  * 
  * ------------------------------------------------------------
+ * 
+ * METHODS TO TEST*(Subject to change. Add or Remove as needed):
+ * 
+ * ---ACCOUNT CLASS---
+ * Account.getBalance();
+ * Account.getCustomerId();
+ * Account.getAccountId();
+ * 
+ * ---BANK CLASS---
+ * Bank.getNAME();
+ * Bank.getAllAccounts();
+ * Bank.addCustomer();
+ * Bank.removeCustomer();
+ * Bank.getCustomer();
+ * Bank.getAllCustomers();
+ * 
+ * ---CUSTOMER CLASS---
+ * Customer.getCustomerId();
+ * Customer.getBank();
+ * Customer.getFirstName();
+ * Customer.getLastName();
+ * Customer.getCustomerAccounts();
+ * 
+ * ---TRANSACTION CLASS---
+ * Transaction.getId();
+ * Transaction.getType();
+ * Transaction.getAmount();
+ * Transaction.getDescription();
+ * Transaction.getTimestamp();
+ * 
+ * 
+ * ------------------------------------------------------------
  */
 
 package testing;
 
-import banking.Account;
-import banking.Bank;
-import banking.Customer;
-import banking.SavingsAccount;
-import banking.Transaction;
-import banking.TransactionType;
+import banking.*;
 
 //Comments regarding project at top of class
-public class DBTest {
-	public static void main(String args[]) {
 
-	}
+@SuppressWarnings("unused")
+public class DBTest {
 
 }
