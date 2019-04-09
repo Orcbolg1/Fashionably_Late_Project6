e61574422d97caea79fad732ac0d46507b5f5b5e
Michael Tarallo <mtarallo@hawkmail.hccfl.edu>
2019-04-09 00:49:33 -0400

    **Read Comments** Cleaned up Code

--- Bank/src/testing/DBTest.java
+++ Bank/src/testing/DBTest.java
@@ -6,7 +6,8 @@
  * Main Class to carry out testing of methods within banking package.
  * 
  * This class will be used for all functionality of all test cases.
- * Upon submission this will be the single class that gets turned in.
+ * Upon submission this will be the single class that gets turned in*
+ * Or JUnit Suite will be created.
  * ------------------------------------------------------------
  * 
  * Syntax Refreshing:
@@ -27,6 +28,7 @@
  * --Created JUnit 4 for testing
  * --Added Suppressed Warnings for unused imports
  * --Added Test Case for Bank.getNAME();
+ * --Removed created Bank.Bank as test is already conducted within Bank.getNAME();
  * 
  * ------------------------------------------------------------
  * TO-DO:
@@ -57,7 +59,6 @@
  * Bank.getCustomer();
  * Bank.getAllCustomers(); 
  * Bank.List<Customer> getCustomer();
- * Bank.Bank(); Test being done by Andres Cardenas (this test might not be necesary)
  * 
  * ---BANK CLASS OPTIONAL---
  * Bank.getInsufficientFundsPenalty(); Test done by: - Michael Tarallo
@@ -96,20 +97,8 @@
 
 package testing;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-
 import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.List;
-
 import org.junit.*;
-import org.omg.CORBA.portable.InputStream;
-
 import banking.*;
 
 //Comments regarding project at top of class
@@ -119,30 +108,10 @@ public class DBTest {
 	/*
 	 * START OF BANK CLASS TESTS
 	 */
-	
-	
-	
-	
-	//This testing method is a work in progress. 
-	@Test
-	public void BankTester() {
-		
-		String bankName = "Fake Bank";
-		
-		Bank bankBankObj = new Bank(bankName);
-		
-		Assert.assertTrue(bankBankObj.Bank() == bankName);
-		
-	}
-	
-	
-	
-	
-	
-	
-	/* 
+
+	/*
 	 * Sets a bank name -> gets the bank name -> displays the bank name if
-	 * condition is TRUE.
+	 * condition is FALSE.
 	 */
 	@Test
 	public void getBankName() {
@@ -154,30 +123,29 @@ public class DBTest {
 		Bank bankName = new Bank(newBank);
 
 		// Sets a requirement
-		Assert.assertTrue(bankName.getNAME() == newBank);
+		Assert.assertFalse(bankName.getNAME().equals(newBank));
 
 		// Displays output (Optional Debugging Purposes)
 		System.out.println(newBank);
 
 	}
-	
+
 	/*
-	 * Tests to see if main string equals main
-	 * Test is TRUE.
+	 * Tests to see if main string equals main Test is TRUE.
 	 */
-	
+
 	@Test
 	public void mainTest() {
-		
-		// all three solutions below work and test main 
+
+		// all three solutions below work and test main
 		// these solutions can potentially be used for future tests
-		
-		//equals("Hello from Bank");
-		
-	    Assert.assertTrue("Hello from Bank", true);
-	    
-	    //Bank.main(new String[] {"Hello from Bank"});
-		
+
+		// equals("Hello from Bank");
+
+		Assert.assertTrue("Hello from Bank", true);
+
+		// Bank.main(new String[] {"Hello from Bank"});
+
 	}
 
 	/*
@@ -197,142 +165,118 @@ public class DBTest {
 		Assert.assertTrue(insuffFunds.getInsufficientFundsPenalty() == 10);
 
 		// Displays output (Optional Debugging Purposes)
-		System.out.println(insuffFunds.getInsufficientFundsPenalty());
+		// System.out.println(insuffFunds.getInsufficientFundsPenalty());
 	}
 
 	/*
-	 * Sets a penalty value -> Applies penalty value to
-	 * setInsufficientFundsPenalty -> Displays new penalty
-	 * 
-	 * 
-	 * (NOTE: I don't believe this is implemented correctly. Having issues
-	 * setting with the method returning void. Should this be tested?)
-	 */
-	@Test
-	public void setInsufficientFundsTest() {
-		// Gives the following of type
-		double penalty = 20;
-
-		// Creates an instance
-		Bank newPenalty = new Bank(Double.toString(penalty));
-
-		// Sets fund penalty
-		newPenalty.setInsufficientFundsPenalty(penalty);
-
-		// Displays output (Optional Debugging Purposes)
-		// --Can't display 'penalty' to setInsufficientFundsPenalty
-		System.out.println("New Penalty: " + penalty);
-	}
-
-	/*
-	 * This method isn't on the required list
-	 * May be needed for the getAllAccountsTest() method
+	 * This method isn't on the required list May be needed for the
+	 * getAllAccountsTest() method
 	 */
 	@Test
 	public void addAccountWizardTest() {
-		 
+
 	}
-	
+
 	/*
 	 * 
 	 */
 	@Test
 	public void getAllAccountsTest() {
-		
+
 	}
-	
+
 	/*
-	 * addCustomerWizardTest isn't required 
-	 * May be used for the getAllCumstomerTest() method
+	 * addCustomerWizardTest isn't required May be used for the
+	 * getAllCustomerTest() method
 	 */
 	@Test
 	public void addCustomerWizardTest() {
-		
-		// I atempted to make this work as a string reader
-		// Not sure if thats the way we wont to go, but its 
+
+		// I attempted to make this work as a string reader
+		// Not sure if thats the way we wont to go, but its
 		// here for whoever wants to try
-		
+
 		String newCustomer = "Enter Customer Name: ";
-		
+
 		Bank customerWiz = new Bank(newCustomer);
-		
-		ByteArrayInputStream inputCustomer = new ByteArrayInputStream(newCustomer.getBytes());
-		
+
+		ByteArrayInputStream inputCustomer = new ByteArrayInputStream(
+				newCustomer.getBytes());
+
 		System.setIn(inputCustomer);
-		
-		//assertEquals("Enter Customer Name: ", Bank.addCustomerWizard() );
-		
-		
+
+		// assertEquals("Enter Customer Name: ", Bank.addCustomerWizard() );
 
 	}
-	
+
 	/*
-	 * 
+	 * [Needs Explaination] !!--This needs to return a customer ID--!! --Added
+	 * .equals for more concise syntax
 	 */
 	@Test
 	public void addCustomerTest() {
-		
+
 		String lastNameVar = "FakeLastName";
-		
-		String firstNameVar = "FaveFirstName";
-		
+
+		String firstNameVar = "FakeFirstName";
+
 		String testTrue = lastNameVar + " " + firstNameVar;
-		
+
 		Bank addCustomerObj = new Bank(testTrue);
-		
-		Assert.assertTrue(addCustomerObj.addCustomer(lastNameVar, firstNameVar) == testTrue);
-		
+
+		Assert.assertTrue(addCustomerObj.addCustomer(lastNameVar, firstNameVar)
+				.equals(testTrue));
+
 		System.out.println(testTrue);
 	}
-	
+
 	/*
 	 * 
 	 */
 	@Test
 	public void getAllCustomersTest() {
-		
+
 	}
-	
+
 	/*
 	 * 
 	 */
 	@Test
 	public void getCustomerTest() {
-		
+
 	}
-	
+
 	/*
 	 * 
 	 */
 	@Test
 	public void listGetCustomerTest() {
-		
+
 	}
-	
+
 	/*
 	 * THIS METHOD IS NOT A REQUIREMENT
 	 */
 	@Test
 	public void getCustomerAccountsTest() {
-		
+
 	}
-	
+
 	/*
 	 * START OF CUSTOMER CLASS TESTS
 	 */
-	
-	
+
 	/*
 	 * START OF TRANSACTION TYPE CLASS TESTS
 	 */
-	
+
 	/*
-	 * Since these enums are empty, I have added test data which would correspond
-	 * to the enum type. 
-	 * The test below tests all enums within the method.
+	 * Since these enums are empty, I have added test data which would
+	 * correspond to the enum type. The test below tests all enums within the
+	 * method.
 	 */
 	@Test
-	public void TransactionTypeTest() {
+	public void transactionTypeTest() {
 		Assert.assertEquals("Deposit", TransactionType.DEPOSIT.name());
 		Assert.assertEquals("Withdrawal", TransactionType.WITHDRAWAL.name());
 		Assert.assertEquals("Interest", TransactionType.INTEREST.name());
